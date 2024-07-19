13b9347f67c35266f19f24e0e189719a8a686cf6
danielch04 <ruben.tehuacatl.choncoa.cet146@gmail.com>
2024-07-19 11:30:15 -0600

    CursoCompleto

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'  //sirve para requerir un modulo
+//import './topics/1-basic-types';
+//import './topics/02-object-interface';
+//import './topics/03-functions';
+import './topics/05-basic-destructuring';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  hola mundo
+`;
